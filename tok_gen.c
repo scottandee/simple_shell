b6d6df4ab7a2d7f0776902ce449251e971d299d6
Andee <andeeolayinkascott@gmail.com>
2023-02-14 17:56:34 +0000

    Changes to token generator file

--- tok_gen.c
+++ tok_gen.c
@@ -2,9 +2,9 @@
 
 char **token_gen(char *buffer, char *delim)
 {
-	char **split_string;
-	int i = 0, j, token_num = 0, len;
-	char *str = NULL, *token;
+	char **split_string = NULL;
+	int i = 0, j, token_num = 0, len = 0;
+	char *str = NULL, *token = NULL;
 	str = strdup(buffer);
 	len = strlen(str);
 	while (str[i] != '\0')
@@ -14,23 +14,24 @@ char **token_gen(char *buffer, char *delim)
 		i++;
 	}
 	token_num++;
-	split_string = malloc(sizeof(char) * (len + 1));
+	split_string = malloc(sizeof(char) * len);
 
 	for (j = 0; j < token_num; j++)
 	{
 		if (j == 0)
 		{			
-			token = strtok(str, delim);
-			split_string[j] = malloc(sizeof(char) * (strlen(token) + 1));
-			strcpy(split_string[j], token);
+			token = _strtok(str, delim);
+			split_string[j] = malloc(sizeof(char) * (strlen(token)));
+			split_string[j] = strdup(token);
 		}
 		else
 		{
-			token = strtok(NULL, delim);
-			split_string[j] = malloc(sizeof(char) * (1 + strlen(token)));
-			strcpy(split_string[j], token);
+			token = _strtok(NULL, delim);
+			split_string[j] = malloc(sizeof(char) * (strlen(token)));
+			split_string[j] = strdup(token);
 		}
 	}
+	free(str);
 	split_string[token_num] = NULL;
 	return (split_string);
 }
