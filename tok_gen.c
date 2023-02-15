9f9ae0fd00e4b9c929a96f82acf80e440c00905f
Andee <andeeolayinkascott@gmail.com>
2023-02-15 17:33:34 +0000

    My strcpy and strlen functions implemented

--- tok_gen.c
+++ tok_gen.c
@@ -5,7 +5,7 @@ char **token_gen(char *buffer, char *delim)
 	char **split_string = NULL;
 	int i = 0, j, token_num = 0;
 	char *str = malloc(sizeof(char) * strlen(buffer)), *token;
-	strcpy(str, buffer);
+	_strcpy(str, buffer);
 	while (str[i] != '\0')
 	{
 		if (*(str + i) == ' ')
@@ -20,14 +20,14 @@ char **token_gen(char *buffer, char *delim)
 		if (j == 0)
 		{			
 			token = strtok(str, delim);
-			split_string[j] = malloc(sizeof(char) * (strlen(token)));
-			strcpy(split_string[j], token);
+			split_string[j] = malloc(sizeof(char) * (_strlen(token)));
+			_strcpy(split_string[j], token);
 		}
 		else
 		{
 			token = strtok(NULL, delim);
-			split_string[j] = malloc(sizeof(char) * (strlen(token)));
-			strcpy(split_string[j], token);
+			split_string[j] = malloc(sizeof(char) * (_strlen(token)));
+			_strcpy(split_string[j], token);
 		}
 	}
 	free(str);
