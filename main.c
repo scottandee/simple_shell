386d8c47a92aa399e8afc589055751df0047cfc8
Andee <andeeolayinkascott@gmail.com>
2023-02-18 13:25:21 +0000

    Some changes

--- main.c
+++ main.c
@@ -1,5 +1,10 @@
 #include "shell.h"
-
+/**
+  * main - this is the entry point function
+  * @argc: this is the number of arguments passed
+  * @argv: this is a string of arguments passed
+  * Return: Always 0
+  */
 int main(int argc, char **argv)
 {
 	(void)argc;
