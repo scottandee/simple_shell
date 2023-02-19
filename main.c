6c2eb6a5e273177994e1265360524b97868092eb
Andee <andeeolayinkascott@gmail.com>
2023-02-19 17:33:36 +0000

    Implementation of the get_builtin

--- main.c
+++ main.c
@@ -7,13 +7,15 @@
   */
 int main(int argc, char **argv)
 {
-	(void)argc;
-	(void)argv;
-	char *dollar = "$ ";
 	size_t size = 0;
 	char *buffer = NULL;
 	ssize_t read;
-	int status = 1;
+	int status = 1, built;
+	char *dollar;
+
+	dollar = "$ ";
+	(void)argc;
+	(void)argv;
 
 	while (status)
 	{
@@ -22,10 +24,16 @@ int main(int argc, char **argv)
 		if (read != -1)
 		{
 			argv = token_gen(buffer, " \n");
-			argv[0] = find_path(argv[0]);
-			if (argv[0] == NULL)
-				perror("./shell");
-			execute(argv);
+			built = get_builtin_func(argv);
+			if (built == 0)
+			{
+				status = 0;
+			}
+			if (built == -1)
+			{
+				argv[0] = find_path(argv[0]);
+				execute(argv);
+			}
 		}
 		if (read == -1)
 		{
