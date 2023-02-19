31f74999cee91ecf087e4906a6f2f5f133a07aab
Andee <andeeolayinkascott@gmail.com>
2023-02-19 17:35:25 +0000

    Addition of header files for get_builtin and other buitin functions

--- shell.h
+++ shell.h
@@ -6,12 +6,19 @@
 #include <stdio.h>
 #include <string.h>
 #include <sys/types.h>
+#include <sys/stat.h>
 #include <sys/wait.h>
 
+extern char **environ;
 int execute(char **args);
+int exit_shell(char **args);
+int print_env(char **args);
 char *_strtok(char *str, const char *delim);
+int get_builtin_func(char **args);
 char *_strcpy(char *dest, const char *src);
 size_t _strlen(const char *s);
 char **token_gen(char *buffer, char *delim);
+char *_getenv(const char *name);
+char *find_path(char *command);
 
 #endif /* _SHELL_H */
