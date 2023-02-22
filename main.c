b8aceeea29573943714488685467d1685cd560d3
Andee <andeeolayinkascott@gmail.com>
2023-02-22 08:53:26 +0000

    checking task 1

--- main.c
+++ main.c
@@ -7,7 +7,7 @@
   */
 int main(int argc, char **argv)
 {
-	int status = 1, built;
+	int status = 1; /*built*/;
 
 	while (status)
 	{
@@ -15,16 +15,16 @@ int main(int argc, char **argv)
 
 		argc = 0;
 		argv = token_gen(&argc, input, " \n");
-		built = get_builtin_func(argv);
+		/*built = get_builtin_func(argv);
 		if (built == 0)
 		{
 			status = 0;
 		}
 		if (built == -1)
-		{
+		{*/
 			argv[0] = find_path(argv[0]);
 			execute(argv);
-		}
+		/*}*/
 	}
 	return (0);
 
