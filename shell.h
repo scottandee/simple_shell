6892bc7cbed82f76febfa965e0b1839134476578
Andee <andeeolayinkascott@gmail.com>
2023-02-24 08:21:03 +0000

    Testing task 1

--- shell.h
+++ shell.h
@@ -11,23 +11,23 @@
 
 extern char **environ;
 int execute(char **args);
-char *read_input();
-int exit_shell(char **args);
+/*char *read_input();
+int exit_shell(char **args);*/
 int hsh_loop(void);
-char *removeLeading(char *str);
+/*char *removeLeading(char *str);
 char *removeLagging(char *str);
 void trimleadingandTrailing(char *s);
 int print_env(char **args);
-char *_strtok(char *str, const char *delim);
+char *_strtok(char *str, const char *delim);*/
 char *_strcat(char *dest, const char *src);
-int get_builtin_func(char **args);
-void freeArray(char **arr);
+/*int get_builtin_func(char **args);*/
+/*void freeArray(char **arr);*/
 char *_strcpy(char *dest, const char *src);
 size_t _strlen(const char *s);
-char **token_gen(char *buffer, char *delim);
-char *_getenv(const char *name);
+/*char **token_gen(char *buffer, char *delim);
+char *_getenv(const char *name);*/
 char *_strdup(const char *s);
-char *find_path(char *command);
+/*char *find_path(char *command);*/
 int _strcmp(char *s1, char *s2);
 
 #endif /* _SHELL_H */
